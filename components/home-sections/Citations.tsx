01c705b4a53f3f03e3cc2d28a00095ab81121676
Zach Miller <home@mac-2.local>
2023-01-19 08:38:00 -0500

    twiiter cards

--- components/home-sections/Citations.tsx
+++ components/home-sections/Citations.tsx
@@ -88,15 +88,7 @@ const Citations: React.FC = () => {
           Regularly cited by thought leaders in the space
         </Text>
         <IconsGrid>
-          <Articles>
-          <meta name="twitter:card" content="summary" />
-<meta name="twitter:site" content="@CryptoStats_" />
-<meta name="twitter:title" content="Small Island Developing States Photo Submission" />
-<meta name="twitter:description" content="View the album on Flickr." />
-            
-            
-            {/* <blockquote><p lang="en" dir="ltr">Needs to get under $0.05 to be truly acceptable imo. But we&#39;re definitely making great progress, and even proto-danksharding may be enough to get us there for a while!</p>&mdash; vitalik.eth (@VitalikButerin) <a href="https://twitter.com/VitalikButerin/status/1521501499410587653?ref_src=twsrc%5Etfw">May 3, 2022</a></blockquote> <Script async src="https://platform.twitter.com/widgets.js"></Script>*/}
-            </Articles>
+          <Articles><blockquote className="twitter-tweet"><p lang="en" dir="ltr">Needs to get under $0.05 to be truly acceptable imo. But we&#39;re definitely making great progress, and even proto-danksharding may be enough to get us there for a while!</p>&mdash; vitalik.eth (@VitalikButerin) <a href="https://twitter.com/VitalikButerin/status/1521501499410587653?ref_src=twsrc%5Etfw">May 3, 2022</a></blockquote> <script async src="https://platform.twitter.com/widgets.js"></script></Articles>
           <Articles><blockquote data-conversation="none"><p lang="en" dir="ltr">We can visualize the buy-side demand for blockspace with <a href="https://t.co/GjLI0qNGtS">https://t.co/GjLI0qNGtS</a>. People are paying real money in fees to store things on chain, just like they paid for bandwidth.<br/><br/>These fees are analogous to *revenue* for each L1 chain, and give some basis for coin valuation.</p>&mdash; Balaji (@balajis) <a href="https://twitter.com/balajis/status/1479867973326102528?ref_src=twsrc%5Etfw">January 8, 2022</a></blockquote> <Script async src="https://platform.twitter.com/widgets.js"></Script></Articles>
           <Articles><blockquote  data-conversation="none"><p lang="en" dir="ltr">There are many crypto protocols that have meaningful cash flows. Ethereum generated $31M in fees yesterday. As with SaaS in the early years, the market is trying to predict growth of those cash flows. <a href="https://t.co/0lpULxQexO">https://t.co/0lpULxQexO</a></p>&mdash; cdixon.eth (@cdixon) <a href="https://twitter.com/cdixon/status/1473890484741296130?ref_src=twsrc%5Etfw">December 23, 2021</a></blockquote> <Script async src="https://platform.twitter.com/widgets.js"></Script></Articles>
           <Articles><blockquote data-conversation="none"><p lang="en" dir="ltr">There is a difference between what a dapp generates and what the chain does. Great for jpg. I hope there is a robust NFT ecosystem for him and he makes a ton $$$. Check out <a href="https://t.co/5rJOHkeioQ">https://t.co/5rJOHkeioQ</a>. It&#39;s a usable reference site</p>&mdash; Mark Cuban (@mcuban) <a href="https://twitter.com/mcuban/status/1556853332249026560?ref_src=twsrc%5Etfw">August 9, 2022</a></blockquote> <Script async src="https://platform.twitter.com/widgets.js" ></Script></Articles>
